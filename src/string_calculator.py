0bbebd0205ef52f76c5d80bc0d1890845a5b9db7
M-Pala <madhav2panchhiwala@gmail.com>
2025-07-30 12:04:09 +0530

    support custom delimiters

--- src/string_calculator.py
+++ src/string_calculator.py
@@ -3,5 +3,11 @@ import re
 def add(numbers: str) -> int:
     if not numbers:
         return 0
-    parts = re.split(',|\n', numbers)
+
+    delimiter = ',|\n'
+    if numbers.startswith("//"):
+        delimiter_part, numbers = numbers[2:].split('\n', 1)
+        delimiter = re.escape(delimiter_part)
+
+    parts = re.split(delimiter, numbers)
     return sum(map(int, parts))
