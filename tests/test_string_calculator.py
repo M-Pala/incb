65439bc06832fb50f597d2af5cc7364084dc88e7
M-Pala <madhav2panchhiwala@gmail.com>
2025-07-30 12:27:16 +0530

    multiple delimiters of any length

--- tests/test_string_calculator.py
+++ tests/test_string_calculator.py
@@ -34,3 +34,9 @@ def test_ignore_numbers_greater_than_1000():
 
 def test_delimiters_of_any_length():
     assert add("//[***]\n1***2***3") == 6
+
+def test_multiple_single_char_delimiters():
+    assert add("//[*][%]\n1*2%3") == 6
+
+def test_multiple_multi_char_delimiters():
+    assert add("//[**][%%]\n1**2%%3") == 6
